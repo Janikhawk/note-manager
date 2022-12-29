c0af38ac7d0ad17a6fdce3ae5d5aadfdf581bf17
Zhangeldy <Jangeldy@hotmail.com>
2022-12-29 22:40:13 +0600

    add tags

--- src/pages/create.jsx
+++ src/pages/create.jsx
@@ -3,7 +3,26 @@ import {useNavigate, useParams} from "react-router-dom";
 import {useDispatch} from "react-redux";
 import {createDirectoryAsync} from "../store/directory-slice";
 import {createNoticeAsync} from "../store/notice-slice";
+import { WithContext as ReactTags } from 'react-tag-input';
+import './create.css';
 
+const COUNTRIES = [
+    'KAZAKHSTAN', 'RUSSIA', 'USA'
+];
+
+const suggestions = COUNTRIES.map(country => {
+    return {
+        id: country,
+        text: country
+    };
+});
+
+const KeyCodes = {
+    comma: 188,
+    enter: 13
+};
+
+const delimiters = [KeyCodes.comma, KeyCodes.enter];
 
 export default function CreateNote() {
     const {directoryId = 1} = useParams();
@@ -34,6 +53,35 @@ export default function CreateNote() {
         }
     }
 
+    const [tags, setTags] = useState([
+        { id: 'Thailand', text: 'Thailand' },
+        { id: 'India', text: 'India' },
+        { id: 'Vietnam', text: 'Vietnam' },
+        { id: 'Turkey', text: 'Turkey' }
+    ]);
+
+    const handleDelete = i => {
+        setTags(tags.filter((tag, index) => index !== i));
+    };
+
+    const handleAddition = tag => {
+        setTags([...tags, tag]);
+    };
+
+    const handleDrag = (tag, currPos, newPos) => {
+        const newTags = tags.slice();
+
+        newTags.splice(currPos, 1);
+        newTags.splice(newPos, 0, tag);
+
+        // re-render
+        setTags(newTags);
+    };
+
+    const handleTagClick = index => {
+        console.log('The tag at index ' + index + ' was clicked');
+    };
+
     return (
         <form id="note-form" onSubmit={createDirectory}>
             <label>
@@ -63,6 +111,20 @@ export default function CreateNote() {
                             defaultValue={data.note.description}
                             rows={6}
                             onChange={(event) => setData({ ...data, note: {...data.note, description: event.target.value}})}
+                        />
+                    </label>
+                    <label>
+                        <span>Tags</span>
+                        <ReactTags
+                            tags={tags}
+                            suggestions={suggestions}
+                            delimiters={delimiters}
+                            handleDelete={handleDelete}
+                            handleAddition={handleAddition}
+                            handleDrag={handleDrag}
+                            handleTagClick={handleTagClick}
+                            inputFieldPosition="bottom"
+                            autocomplete
                         />
                     </label>
                 </>
