ef57c258ccf46c62e2effb7bf376bafd51a317e1
Zhangeldy <Jangeldy@hotmail.com>
2022-12-30 22:42:47 +0600

    add dev and prod

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -16,6 +16,10 @@ export default function App() {
     const dispatch = useDispatch();
 
     useEffect(() => {
+        document.title= process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEV_MODE : process.env.REACT_APP_PRO_MODE;
+    })
+
+    useEffect(() => {
         dispatch(getDirectoriesAsync());
         dispatch(getNoticesAsync());
     }, [dispatch])
